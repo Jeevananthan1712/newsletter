61bcd3e075c60f95468d1052f5b835332f42cef9
JEEVA <sjeevananthan1712@gmail.com>
2021-07-18 23:26:09 +0530

    added procfile

--- app.js
+++ app.js
@@ -92,8 +92,14 @@ app.get("/posts/:postId", function(req, res){
 });
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port , function() {
+  console.log("Server has started successfully");
 });
+
+// https://powerful-brushlands-81617.herokuapp.com/ use this to open in website
